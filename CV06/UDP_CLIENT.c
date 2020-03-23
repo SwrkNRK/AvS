8eb0e46dad0079df47f14b330544729f25db7b46
SwrkNRK <root@debian.netlab.kis.fri.uniza.sk>
2020-03-23 10:36:25 +0100

    CV07.01

--- CV06/UDP_CLIENT.c
+++ CV06/UDP_CLIENT.c
@@ -3,47 +3,54 @@
 #include <string.h>
 #include <unistd.h>
 
-#include<sys/types.h>
-#include<sys/socket.h>
-
-#include<netinet/ip.h>
-#include<netinet/in.h>
-
-#include<arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 1234
-#define IP_ADDR "127.0.0.1"
+#define IP_ADDR "10.123.123.255"
 
-int main() {
+int main()
+{
+	int sock;
+	struct sockaddr_in addr;
 
-    int sock;
-    struct sockaddr_in addr;
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sock == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock == -1) {
-        perror("Socket error: ");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    if(inet_aton(IP_ADDR, &addr.sin_addr) == 0) {
-        printf("ERROR: inet_aton: ");
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	if(inet_aton(IP_ADDR, &addr.sin_addr) == 0)
+	{
+		printf("ERROR: inet_aton");
+		close(sock);
+		exit(EXIT_FAILURE);
+	}
+    
+    int allowBroadcast = 1;
+    if(setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &allowBroadcast, sizeof(allowBroadcast)) == -1){
+        perror("setsockopt");
         close(sock);
         exit(EXIT_FAILURE);
     }
-    
-        char buf[1000];
-        memset(buf, '\0', sizeof(buf));
-        int addr_len = sizeof(addr);
-        fgets(buf, 1000, stdin);
-        
 
-        sendto(sock, buf, strlen(buf), 0, (struct sockaddr *) &addr, &addr_len);
-        
 
-    
+	char buf[1000];
+	memset(buf, '\0', 1000);
+	int addr_len = sizeof(addr);
+	fgets(buf, 1000, stdin);
 
-    return EXIT_SUCCESS;
+
+	sendto(sock, buf, strlen(buf), 0, (struct sockaddr *) &addr, addr_len);
+
+
+
+
+	return EXIT_SUCCESS;
 }
--- CV06/UDP_SERVER.c
+++ CV06/UDP_SERVER.c
@@ -3,55 +3,58 @@
 #include <string.h>
 #include <unistd.h>
 
-#include<sys/types.h>
-#include<sys/socket.h>
-
-#include<netinet/ip.h>
-#include<netinet/in.h>
-
-#include<arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 1234
-#define IP_ADDR "127.0.0.1"
+#define IP_ADDR "10.123.123.242"
 
-int main() {
+int main()
+{
+	int sock;
+	struct sockaddr_in addr;
 
-    int sock;
-    struct sockaddr_in addr;
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sock == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock == -1) {
-        perror("Socket error: ");
-        exit(EXIT_FAILURE);
-    }
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	if(inet_aton(IP_ADDR, &addr.sin_addr) == 0)
+	{
+		printf("ERROR: inet_aton");
+		close(sock);
+		exit(EXIT_FAILURE);
+	}
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    if(inet_aton(IP_ADDR, &addr.sin_addr) == 0) {
-        perror("inet_aton: ");
-        close(sock);
-        exit(EXIT_FAILURE);
-    }
+	if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	{
+		perror("bind");
+		close(sock);
+		exit(EXIT_FAILURE);
+	}
 
-    if(bind(sock, (const struct sockaddr *) &addr, sizeof(addr)) == -1){
-    perror("bind");
-    close(sock);
-    exit(EXIT_FAILURE);
-    }
+	for(;;)
+	{
+		char buf[1000];
+		memset(buf, '\0', 1000);
+		int addr_len = sizeof(addr);
+		recvfrom(sock, buf, 1000, 0, (struct sockaddr *) &addr, &addr_len);
+		char *ip = inet_ntoa(addr.sin_addr);
 
-    for(;;) {
-        char buf[1000];
-        memset(buf, '\0', sizeof(buf));
-        int addr_len = sizeof(addr);
-        recvfrom(sock, buf, 1000, 0, (struct sockaddr *) &addr, &addr_len);
-        char *ip = inet_ntoa(addr.sin_addr);
+		printf("Message from: %s:%d text: %s",
+				ip,
+				ntohs(addr.sin_port),
+				buf);
+	}
 
-        printf("Message from: %s:%d text: %s",
-            ip,
-            ntohs(addr.sin_port),
-            buf);
-    }
 
-    return EXIT_SUCCESS;
+
+	return EXIT_SUCCESS;
 }
